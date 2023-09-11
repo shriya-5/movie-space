176224d4f163c0f34a0d37d1d3af7e8249de1319
Shriya S <82114613+shriya-5@users.noreply.github.com>
2023-09-11 09:13:02 +0530

    Add files via upload

---
+++ movies/src/main/java/com/shri/movies/movies/MoviesspaceApplication.java
@@ -0,0 +1,32 @@
+package com.shri.movies.movies;
+
+import  org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+
+public class MoviesspaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesspaceApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+						.allowedOrigins("*")
+						.allowedMethods("*")
+						.allowedHeaders("*")
+						.allowCredentials(false).maxAge(3600);
+			}
+		};
+	}
+
+}
---
+++ movies/src/test/java/com/shri/movies/movies/MoviesspaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shri.movies.movies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesspaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
